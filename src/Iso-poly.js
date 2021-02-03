9f8e55fd9788bfb4a8b7dcff95182a4a41b6e98d
trevsm <trevsm@pm.me>
2021-02-03 13:18:28 -0700

    Added Hover Popups

--- src/Iso-poly.js
+++ src/Iso-poly.js
@@ -4,23 +4,30 @@ import { useThree } from 'react-three-fiber'
 
 function Chair({ materials, nodes }) {
   const [label, setLabel] = useState(false)
+
+  useEffect(() => {
+    document.body.style.cursor = label ? 'pointer' : 'auto'
+  }, [label])
+
   return (
-    <group
-      onPointerEnter={() => {
-        setLabel(true)
-      }}
-      onPointerLeave={() => {
-        setLabel(false)
-      }}
-    >
-      <mesh
-        material={materials.Default_Color}
-        geometry={nodes.Chair_1.geometry}
-      />
-      <mesh
-        material={materials.Default_White}
-        geometry={nodes.Chair_2.geometry}
-      />
+    <group>
+      <group
+        onPointerEnter={() => {
+          setLabel(true)
+        }}
+        onPointerLeave={() => {
+          setLabel(false)
+        }}
+      >
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Chair.geometry}
+        />
+        <mesh
+          material={materials.Default_White}
+          geometry={nodes.Chair_1.geometry}
+        />
+      </group>
       <group visible={label}>
         <mesh
           material={materials.Default_Color}
@@ -30,6 +37,18 @@ function Chair({ materials, nodes }) {
           material={materials.Default_White}
           geometry={nodes.Chair_Label_2.geometry}
         />
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Chair_Text.geometry}
+          position={[-0.06, 0.83, 0.14]}
+          rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+        >
+          <mesh
+            material={materials.Default_Color}
+            geometry={nodes.Chair_CTV.geometry}
+            position={[0, 0, 0.04]}
+          />
+        </mesh>
       </group>
     </group>
   )
@@ -37,23 +56,30 @@ function Chair({ materials, nodes }) {
 
 function Lamp({ materials, nodes }) {
   const [label, setLabel] = useState(false)
+
+  useEffect(() => {
+    document.body.style.cursor = label ? 'pointer' : 'auto'
+  }, [label])
+
   return (
-    <group
-      onPointerEnter={() => {
-        setLabel(true)
-      }}
-      onPointerLeave={() => {
-        setLabel(false)
-      }}
-    >
-      <mesh
-        material={materials.Default_Color}
-        geometry={nodes.Hood_1.geometry}
-      />
-      <mesh
-        material={materials.Default_White}
-        geometry={nodes.Hood_2.geometry}
-      />
+    <group>
+      <group
+        onPointerEnter={() => {
+          setLabel(true)
+        }}
+        onPointerLeave={() => {
+          setLabel(false)
+        }}
+      >
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Hood_1.geometry}
+        />
+        <mesh
+          material={materials.Default_White}
+          geometry={nodes.Hood_2.geometry}
+        />
+      </group>
       <group visible={label}>
         <mesh
           material={materials.Default_Color}
@@ -63,6 +89,18 @@ function Lamp({ materials, nodes }) {
           material={materials.Default_White}
           geometry={nodes.Lamp_Label_2.geometry}
         />
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Lamp_Text.geometry}
+          position={[-0.68, 0.88, 0.1]}
+          rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+        >
+          <mesh
+            material={materials.Default_Color}
+            geometry={nodes.Lamp_CTV.geometry}
+            position={[-0.01, 0, 0.04]}
+          />
+        </mesh>
       </group>
     </group>
   )
@@ -70,20 +108,30 @@ function Lamp({ materials, nodes }) {
 
 function Soda({ materials, nodes }) {
   const [label, setLabel] = useState(false)
+
+  useEffect(() => {
+    document.body.style.cursor = label ? 'pointer' : 'auto'
+  }, [label])
+
   return (
-    <group
-      onPointerEnter={() => {
-        setLabel(true)
-      }}
-      onPointerLeave={() => {
-        setLabel(false)
-      }}
-    >
-      <mesh material={materials.Default_White} geometry={nodes.Can.geometry} />
-      <mesh
-        material={materials.Default_Color}
-        geometry={nodes.Can_1.geometry}
-      />
+    <group>
+      <group
+        onPointerEnter={() => {
+          setLabel(true)
+        }}
+        onPointerLeave={() => {
+          setLabel(false)
+        }}
+      >
+        <mesh
+          material={materials.Default_White}
+          geometry={nodes.Can.geometry}
+        />
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Can_1.geometry}
+        />
+      </group>
       <group visible={label}>
         <mesh
           material={materials.Default_Color}
@@ -93,6 +141,18 @@ function Soda({ materials, nodes }) {
           material={materials.Default_White}
           geometry={nodes.Soda_Label_2.geometry}
         />
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Soda_Text.geometry}
+          position={[-0.59, 0.75, -0.52]}
+          rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+        >
+          <mesh
+            material={materials.Default_Color}
+            geometry={nodes.Soda_CTV.geometry}
+            position={[-0.01, 0, 0.04]}
+          />
+        </mesh>
       </group>
     </group>
   )
@@ -100,51 +160,79 @@ function Soda({ materials, nodes }) {
 
 function Blinds({ materials, nodes }) {
   const [label, setLabel] = useState(false)
+
+  useEffect(() => {
+    document.body.style.cursor = label ? 'pointer' : 'auto'
+  }, [label])
+
   return (
-    <group
-      onPointerEnter={() => {
-        setLabel(true)
-      }}
-      onPointerLeave={() => {
-        setLabel(false)
-      }}
-    >
-      <mesh material={materials.Default_White} geometry={nodes.Blinds.geometry}>
-        <group visible={label}>
+    <group>
+      <mesh
+        visible={false}
+        onPointerEnter={() => {
+          setLabel(true)
+        }}
+        onPointerLeave={() => {
+          setLabel(false)
+        }}
+        material={materials.Material}
+        geometry={nodes.Blinds_HitBox.geometry}
+      />
+      <mesh
+        material={materials.Default_White}
+        geometry={nodes.Blinds.geometry}
+      />
+      <group visible={label}>
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Blinds_Label_1.geometry}
+        />
+        <mesh
+          material={materials.Default_White}
+          geometry={nodes.Blinds_Label_2.geometry}
+        />
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Blinds_Text.geometry}
+          position={[-0.54, 1.02, -0.9]}
+          rotation={[Math.PI / 2, 0, 0]}
+        >
           <mesh
             material={materials.Default_Color}
-            geometry={nodes.Blinds_Label_1.geometry}
+            geometry={nodes.Blinds_CTV.geometry}
+            position={[0.01, 0, 0.04]}
           />
-          <mesh
-            material={materials.Default_White}
-            geometry={nodes.Blinds_Label_2.geometry}
-          />
-        </group>
-      </mesh>
+        </mesh>
+      </group>
     </group>
   )
 }
 
 function Computer({ materials, nodes }) {
   const [label, setLabel] = useState(false)
+
+  useEffect(() => {
+    document.body.style.cursor = label ? 'pointer' : 'auto'
+  }, [label])
+
   return (
-    <group
-      onPointerEnter={() => {
-        setLabel(true)
-      }}
-      onPointerLeave={() => {
-        setLabel(false)
-      }}
-    >
-      <mesh
-        material={materials.Default_White}
-        geometry={nodes.Plane.geometry}
-      />
-      <mesh
-        material={materials.Default_Color}
-        geometry={nodes.Plane_1.geometry}
-      />
-      <group>
+    <group>
+      <group
+        onPointerEnter={() => {
+          setLabel(true)
+        }}
+        onPointerLeave={() => {
+          setLabel(false)
+        }}
+      >
+        <mesh
+          material={materials.Default_White}
+          geometry={nodes.Computer_1.geometry}
+        />
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Computer_2.geometry}
+        />
         <mesh
           material={materials.Default_White}
           geometry={nodes.Cube.geometry}
@@ -163,6 +251,18 @@ function Computer({ materials, nodes }) {
           material={materials.Default_White}
           geometry={nodes.Laptop_Label_2.geometry}
         />
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Laptop_Text.geometry}
+          position={[-0.64, 0.73, -0.19]}
+          rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+        >
+          <mesh
+            material={materials.Default_Color}
+            geometry={nodes.Laptop_CTV.geometry}
+            position={[0.01, 0, 0.04]}
+          />
+        </mesh>
       </group>
     </group>
   )
@@ -170,27 +270,34 @@ function Computer({ materials, nodes }) {
 
 function Garbage({ materials, nodes }) {
   const [label, setLabel] = useState(false)
+
+  useEffect(() => {
+    document.body.style.cursor = label ? 'pointer' : 'auto'
+  }, [label])
+
   return (
-    <group
-      onPointerEnter={() => {
-        setLabel(true)
-      }}
-      onPointerLeave={() => {
-        setLabel(false)
-      }}
-    >
-      <mesh
-        material={materials.Default_Color}
-        geometry={nodes.Bin_1.geometry}
-      />
-      <mesh
-        material={materials.Default_White}
-        geometry={nodes.Bin_2.geometry}
-      />
-      <mesh
-        material={materials.Default_White}
-        geometry={nodes.Garbage.geometry}
-      />
+    <group>
+      <group
+        onPointerEnter={() => {
+          setLabel(true)
+        }}
+        onPointerLeave={() => {
+          setLabel(false)
+        }}
+      >
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Bin_1.geometry}
+        />
+        <mesh
+          material={materials.Default_White}
+          geometry={nodes.Bin_2.geometry}
+        />
+        <mesh
+          material={materials.Default_White}
+          geometry={nodes.Garbage.geometry}
+        />
+      </group>
       <group visible={label}>
         <mesh
           material={materials.Default_Color}
@@ -200,6 +307,18 @@ function Garbage({ materials, nodes }) {
           material={materials.Default_White}
           geometry={nodes.Garbage_Label_2.geometry}
         />
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Garbage_Text.geometry}
+          position={[-0.59, 0.44, 0.67]}
+          rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+        >
+          <mesh
+            material={materials.Default_Color}
+            geometry={nodes.Garbage_CTV.geometry}
+            position={[0.02, 0, 0.04]}
+          />
+        </mesh>
       </group>
     </group>
   )
@@ -221,16 +340,8 @@ function Room({ materials, nodes }) {
 }
 
 function Rug({ materials, nodes }) {
-  const [label, setLabel] = useState(false)
   return (
-    <group
-      onPointerEnter={() => {
-        setLabel(true)
-      }}
-      onPointerLeave={() => {
-        setLabel(false)
-      }}
-    >
+    <group>
       <mesh
         material={materials.Default_Color}
         geometry={nodes.Rug_1.geometry}
@@ -239,40 +350,35 @@ function Rug({ materials, nodes }) {
         material={materials.Default_White}
         geometry={nodes.Rug_2.geometry}
       />
-      <group visible={label}>
-        <mesh
-          material={materials.Default_Color}
-          geometry={nodes.Rug_Label_1.geometry}
-        />
-        <mesh
-          material={materials.Default_White}
-          geometry={nodes.Rug_Label_2.geometry}
-        />
-      </group>
     </group>
   )
 }
 
 function Books({ materials, nodes }) {
   const [label, setLabel] = useState(false)
+
+  useEffect(() => {
+    document.body.style.cursor = label ? 'pointer' : 'auto'
+  }, [label])
+
   return (
-    <group
-      onPointerEnter={() => {
-        setLabel(true)
-      }}
-      onPointerLeave={() => {
-        setLabel(false)
-      }}
-    >
-      <mesh
-        material={materials.Default_Color}
-        geometry={nodes.Book_02.geometry}
-      />
-      <mesh
-        material={materials.Default_White}
-        geometry={nodes.Book_02_1.geometry}
-      />
-      <group>
+    <group>
+      <group
+        onPointerEnter={() => {
+          setLabel(true)
+        }}
+        onPointerLeave={() => {
+          setLabel(false)
+        }}
+      >
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Book_02.geometry}
+        />
+        <mesh
+          material={materials.Default_White}
+          geometry={nodes.Book_02_1.geometry}
+        />
         <mesh
           material={materials.Default_Color}
           geometry={nodes.Book_01_1.geometry}
@@ -291,6 +397,18 @@ function Books({ materials, nodes }) {
           material={materials.Default_White}
           geometry={nodes.Books_Label_2.geometry}
         />
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Books_Text.geometry}
+          position={[-0.81, 0.71, -0.59]}
+          rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+        >
+          <mesh
+            material={materials.Default_Color}
+            geometry={nodes.Books_CTV.geometry}
+            position={[0.03, 0, 0.04]}
+          />
+        </mesh>
       </group>
     </group>
   )
@@ -298,15 +416,24 @@ function Books({ materials, nodes }) {
 
 function WallArt({ materials, nodes }) {
   const [label, setLabel] = useState(false)
+
+  useEffect(() => {
+    document.body.style.cursor = label ? 'pointer' : 'auto'
+  }, [label])
+
   return (
-    <group
-      onPointerEnter={() => {
-        setLabel(true)
-      }}
-      onPointerLeave={() => {
-        setLabel(false)
-      }}
-    >
+    <group>
+      <mesh
+        visible={false}
+        material={materials.Material}
+        geometry={nodes.Art_HitBox.geometry}
+        onPointerEnter={() => {
+          setLabel(true)
+        }}
+        onPointerLeave={() => {
+          setLabel(false)
+        }}
+      />
       <mesh
         material={materials.Default_White}
         geometry={nodes.Wall_Art_1.geometry}
@@ -324,6 +451,18 @@ function WallArt({ materials, nodes }) {
           material={materials.Default_White}
           geometry={nodes.Art_Label_2.geometry}
         />
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Art_Text.geometry}
+          position={[-0.77, 0.58, 0.84]}
+          rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+        >
+          <mesh
+            material={materials.Default_Color}
+            geometry={nodes.Art_CTV.geometry}
+            position={[0.02, 0, 0.04]}
+          />
+        </mesh>
       </group>
     </group>
   )
@@ -331,23 +470,38 @@ function WallArt({ materials, nodes }) {
 
 function Clock({ materials, nodes }) {
   const [label, setLabel] = useState(false)
+
+  useEffect(() => {
+    document.body.style.cursor = label ? 'pointer' : 'auto'
+  }, [label])
+
   return (
-    <group
-      onPointerEnter={() => {
-        setLabel(true)
-      }}
-      onPointerLeave={() => {
-        setLabel(false)
-      }}
-    >
-      <mesh
-        material={materials.Default_Color}
-        geometry={nodes.cbody.geometry}
-      />
-      <mesh
-        material={materials.Default_White}
-        geometry={nodes.cbody_1.geometry}
-      />
+    <group>
+      <group
+        onPointerEnter={() => {
+          setLabel(true)
+        }}
+        onPointerLeave={() => {
+          setLabel(false)
+        }}
+      >
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.cbody.geometry}
+        />
+        <mesh
+          material={materials.Default_White}
+          geometry={nodes.cbody_1.geometry}
+        />
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Large_Hand.geometry}
+        />
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Small_Hand.geometry}
+        />
+      </group>
       <group visible={label}>
         <mesh
           material={materials.Default_Color}
@@ -357,45 +511,69 @@ function Clock({ materials, nodes }) {
           material={materials.Default_White}
           geometry={nodes.Clock_Label_2.geometry}
         />
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Clock_Text.geometry}
+          position={[0.59, 1.35, -0.9]}
+          rotation={[Math.PI / 2, 0, 0]}
+        >
+          <mesh
+            material={materials.Default_Color}
+            geometry={nodes.Clock_CTV.geometry}
+            position={[-0.01, 0, 0.04]}
+          />
+        </mesh>
       </group>
-      <mesh
-        material={materials.Default_Color}
-        geometry={nodes.Large_Hand.geometry}
-      />
-      <mesh
-        material={materials.Default_Color}
-        geometry={nodes.Small_Hand.geometry}
-      />
     </group>
   )
 }
 
 function Glasses({ materials, nodes }) {
   const [label, setLabel] = useState(false)
+
+  useEffect(() => {
+    document.body.style.cursor = label ? 'pointer' : 'auto'
+  }, [label])
+
   return (
-    <group
-      onPointerEnter={() => {
-        setLabel(true)
-      }}
-      onPointerLeave={() => {
-        setLabel(false)
-      }}
-    >
+    <group>
+      <mesh
+        visible={false}
+        material={materials.Material}
+        geometry={nodes.Glasses_HitBox.geometry}
+        onPointerEnter={() => {
+          setLabel(true)
+        }}
+        onPointerLeave={() => {
+          setLabel(false)
+        }}
+      />
       <mesh
         material={materials.Default_White}
-        geometry={nodes.Sunglasses.geometry}
-      >
-        <group visible={label}>
+        geometry={nodes.Glasses.geometry}
+      />
+      <group visible={label}>
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Glasses_Label_1.geometry}
+        />
+        <mesh
+          material={materials.Default_White}
+          geometry={nodes.Glasses_Label_2.geometry}
+        />
+        <mesh
+          material={materials.Default_Color}
+          geometry={nodes.Glasses_Text.geometry}
+          position={[-0.72, 0.69, -0.42]}
+          rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+        >
           <mesh
             material={materials.Default_Color}
-            geometry={nodes.Glasses_Label_1.geometry}
+            geometry={nodes.Glasses_CTV.geometry}
+            position={[0.01, 0, 0.04]}
           />
-          <mesh
-            material={materials.Default_White}
-            geometry={nodes.Glasses_Label_2.geometry}
-          />
-        </group>
-      </mesh>
+        </mesh>
+      </group>
     </group>
   )
 }
@@ -430,11 +608,11 @@ function Desk({ materials, nodes }) {
     <group>
       <mesh
         material={materials.Default_White}
-        geometry={nodes.Cylinder.geometry}
+        geometry={nodes.Desk_1.geometry}
       />
       <mesh
         material={materials.Default_Color}
-        geometry={nodes.Cylinder_1.geometry}
+        geometry={nodes.Desk_2.geometry}
       />
     </group>
   )
@@ -456,7 +634,7 @@ export default function Model(props) {
     <group
       ref={group}
       dispose={null}
-      position={[0, -0.73, 0]}
+      position={[0, -0.76, 0]}
       rotation={[0, -0.8, 0]}
     >
       <TextHeaders nodes={nodes} materials={materials} />
